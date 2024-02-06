2b8de6fb03ef5d2be82cac8938655b18c81a76a1
nakamoto <zksytmkn4869@gmail.com>
2024-02-06 12:39:26 +0900

    🎨

--- prisma/migrations/20240206033750_init/migration.sql
+++ prisma/migrations/20240206033750_init/migration.sql
@@ -1,10 +1,21 @@
 -- CreateTable
+CREATE TABLE "User" (
+    "id" INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT,
+    "name" TEXT NOT NULL,
+    "password" TEXT NOT NULL
+);
+
+-- CreateTable
 CREATE TABLE "Todo" (
     "id" INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT,
     "createdAt" DATETIME NOT NULL DEFAULT CURRENT_TIMESTAMP,
     "updatedAt" DATETIME NOT NULL,
     "title" TEXT NOT NULL,
     "progress" TEXT NOT NULL,
+    "bookmark" BOOLEAN NOT NULL DEFAULT false,
     "userId" INTEGER NOT NULL,
     CONSTRAINT "Todo_userId_fkey" FOREIGN KEY ("userId") REFERENCES "User" ("id") ON DELETE RESTRICT ON UPDATE CASCADE
 );
+
+-- CreateIndex
+CREATE UNIQUE INDEX "User_name_key" ON "User"("name");
